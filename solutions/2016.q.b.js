4f1c687da7b98f63334501e2a4cd59e5cf59d643
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-03-20 09:52:08 +0100

    Basic cases covered

--- solutions/2016.q.b.js
+++ solutions/2016.q.b.js
@@ -1,6 +1,7 @@
+let _ = require('lodash')
+
 module.exports = function(input, output) {
   let t = input.popInt()
-
   for(var i = 0 ; i < t ; i++) {
     var stack = input.popString()
     var solution = getSolutionFor(stack)
@@ -9,5 +10,28 @@ module.exports = function(input, output) {
 };
 
 function getSolutionFor(stack) {
+  var currentStack = _.trimEnd(stack, '+')
+  var isBasicCase = checkBasicCases(stack)
+  if (isBasicCase >= 0) {
+    return isBasicCase
+  }
+
+  //end char is -
+
   return 1
+}
+
+function checkBasicCases(stack) {
+  if (stack.length === 0) {
+    return 0
+  }
+  if (stack.indexOf('-') < 0) {
+    //only +
+    return 0
+  }
+  if (stack.indexOf('+') < 0) {
+    //only -
+    return 1
+  }
+  return -1;
 }
--- test/2016.q.b-test.js
+++ test/2016.q.b-test.js
@@ -4,15 +4,33 @@ let expect = require('chai').expect
 let solution = require('../solutions/2016.q.b.js')
 
 describe('2016.q.b', () => {
-  var input, output
+  var output
 
-  before(() => {
+  beforeEach(() => {
     output = new Output()
   })
 
-  it('Should flip 1 pancake', () => {
-    input = new Input(['1', '-'])
+  it('All +: 0', () => {
+    let input = new Input(['1', '++++++'])
+    solution(input, output)
+    expect(output.toString()).to.equal('Case #1: 0')
+  })
+
+  it('All -: 1', () => {
+    let input = new Input(['1', '-----'])
     solution(input, output)
     expect(output.toString()).to.equal('Case #1: 1')
+  })
+
+  it('---+++: 1', () => {
+    let input = new Input(['1', '---+++'])
+    solution(input, output)
+    expect(output.toString()).to.equal('Case #1: 1')
+  })
+
+  it('+++---: 2', () => {
+    let input = new Input(['1', '---+++'])
+    solution(input, output)
+    expect(output.toString()).to.equal('Case #1: 2')
   })
 })
