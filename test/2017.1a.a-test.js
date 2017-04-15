5c45d3e072310881491000a97d6b48af86fc8e6b
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-15 11:41:24 +0200

    2017.1a.a - broken tests

--- test/2017.1a.a-test.js
+++ test/2017.1a.a-test.js
@@ -10,9 +10,26 @@ describe('2017.1a.a', () => {
     output = new Output()
   })
 
-  it('Should ...', () => {
-    let input = new Input(['1', '0'])
+  it('Should handle rectangles', () => {
+    let input = new Input(['1', '2 2', '??', '?A'])
     solution(input, output)
-    expect(output.toString()).to.equal('Case #1: 0')
+    expect(output.toString()).to.equal(
+      'Case #1:\n'+
+      'AA\n'+
+      'AA'
+    )
+  })
+
+
+  it('Should handle rectangles', () => {
+    let input = new Input(['1', '4 3', '?DE', '?AB', '??C', '???'])
+    solution(input, output)
+    expect(output.toString()).to.equal(
+      'Case #1:\n'+
+      'DDE\n' +
+      'AAB\n' +
+      'CCC\n' +
+      'CCC'
+    )
   })
 })
